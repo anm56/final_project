e51e16f425651ce8d8fd6f7bc47dc459886c90f4
Anshul N Maathur <anm56@afsaccess1.njit.edu>
2017-12-05 18:42:19 -0500

    register page

---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<p><b><h1> To-Do Application</h1></b></p>
+<body>
+<form action="/action_page.php" method="post" enctype="multipart/form-data">
+First Name : <input type="text" name="fname" placeholder="First name"><br><br>
+Last Name : <input type="text" name="lname" placeholder="Last name"><br><br>
+Email : <input type="email" name="email" placeholder="Email"><br><br>
+Username : <input type="text" name="Username" placeholder="Username"><br><br>
+Password : <input type="password" name="psw" placeholder="Password"><br><br>
+Confirm Password : <input type="password" name="psw" placeholder="Confirm password"><br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
+
