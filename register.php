681584ffe727383cc8e70197266617d439a33f1b
Anshul N Maathur <anm56@afsaccess1.njit.edu>
2017-12-06 18:16:42 -0500

    reg page updated

--- register.php
+++ register.php
@@ -6,6 +6,10 @@
 First Name : <input type="text" name="fname" placeholder="First name"><br><br>
 Last Name : <input type="text" name="lname" placeholder="Last name"><br><br>
 Email : <input type="email" name="email" placeholder="Email"><br><br>
+Phone : <input type="tel" name="tel" placeholder="123-4567890" maxlength="10"><br><br>
+Birthday : <input type="date" name="bday" placeholder="mm/dd/yyyy"><br><br>
+Gender : <input type="radio" name="gender" value="Male">Male
+         <input type="radio" name="gender" value="Female">Female<br><br>
 Username : <input type="text" name="Username" placeholder="Username"><br><br>
 Password : <input type="password" name="psw" placeholder="Password"><br><br>
 Confirm Password : <input type="password" name="psw" placeholder="Confirm password"><br><br>
