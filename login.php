8d6d9daccef8c4a45fd76f9a62b8230dce2c1815
Anshul N Maathur <anm56@afsaccess1.njit.edu>
2017-12-05 19:11:35 -0500

    login page

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<p><b><h2>LogIn</h2></b></p>
+<body>
+<form action="/action_page.php" method="post" enctype="multipart/form-data">
+First Name : <input type="text" name="fname" placeholder="First name"><br><br>
+Last Name : <input type="text" name="lname" placeholder="Last name"><br><br>
+Email : <input type="email" name="email" placeholder="Email"><br><br>
+Password : <input type="password" name="psw" placeholder="Password"><br><br>
+<input type="submit" value="LogIn">
+<p>Not Registered? New user? </p> <a href= "register.php"><h2>SignUp</h2></a>
+</form>
+</body>
+</html>
