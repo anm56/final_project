ba3974a29d50a0302b05d9555db14b5632782e37
Anshul N Maathur <anm56@afsaccess1.njit.edu>
2017-12-11 19:31:22 -0500

    update

--- database.php
+++ database.php
@@ -1,18 +1,10 @@
 <?php
+ini_set('display_errors','On');
+error_reporting(E_ALL);
 $servername = "sql1.njit.edu";
 $username = "anm56";
 $password = "anshul2311";
 
-try {
-    $conn= new PDO("mysql:host=$servername;dbname=anm56", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    echo "<br>"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
 $fname = $_POST['fname']; 
 $lname = $_POST['lname'];
 $email = $_POST['email'];
@@ -20,6 +12,23 @@ $phone = $_POST['tel'];
 $bday = $_POST['bday'];
 $gender = $_POST['gender'];
 $password = $_POST['psw'];
+
+try {
+    $conn= new PDO("mysql:host=$servername;dbname=anm56", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    echo "<br>"; 
+    $sql= $con->prepare("INSERT INTO 'accounts' (fname, lname, email, phone, bday, gender, password) 
+                               VALUES ('$fname', '$lname', '$email', 
+                                                 '$phone','$bday', '$gender', '$password')"); 
+   $sql->execute();
+   $results=$sql->fetchAll();
+
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
     
 /*$query = "SELECT * FROM `accounts` WHERE `email` = '$email'";
 $sqlsearch = mysql_query($query);
@@ -40,9 +49,6 @@ if ($resultcount > 0) {
     
 } else {*/
 
-    $sql= "INSERT INTO 'accounts' (fname, lname, email, phone, bday, gender, password) 
-                               VALUES ('$fname', '$lname', '$email', 
-                                                 '$phone','$bday', '$gender', '$password')"; 
 
   //  or die(mysql_error());  
 
