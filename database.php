fe82d5fdea0c5212205ac0c4398b3d23857a912b
Anshul N Maathur <anm56@afsaccess1.njit.edu>
2017-12-11 20:44:19 -0500

    new update

--- database.php
+++ database.php
@@ -11,46 +11,21 @@ $email = $_POST['email'];
 $phone = $_POST['tel'];
 $bday = $_POST['bday'];
 $gender = $_POST['gender'];
-$password = $_POST['psw'];
+$pass = $_POST['psw'];
 
 try {
     $conn= new PDO("mysql:host=$servername;dbname=anm56", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
     echo "<br>"; 
-    $sql= $con->prepare("INSERT INTO 'accounts' (fname, lname, email, phone, bday, gender, password) 
-                               VALUES ('$fname', '$lname', '$email', 
-                                                 '$phone','$bday', '$gender', '$password')"); 
+    $sql= $conn->prepare("INSERT INTO accounts (fname, lname, email, phone, birthday, gender, password) VALUES ('$fname', '$lname', '$email', 
+                                                 '$phone','$bday', '$gender', '$pass')"); 
    $sql->execute();
-   $results=$sql->fetchAll();
+  // $results=$sql->fetchAll();
 
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-    
-/*$query = "SELECT * FROM `accounts` WHERE `email` = '$email'";
-$sqlsearch = mysql_query($query);
-$resultcount = mysql_num_rows($sqlsearch);
-
-if ($resultcount > 0) {
- 
-    mysql_query("UPDATE `accounts` SET 
-                                `fname` = '$fname',
-                                `lname`='$lname',
-                                `email` = '$email',
-                                `phone` = '$phone',
-                                `bday` = '$bday',
-                                `gender`='$gender',
-                                `password` = '$password'        
-                             WHERE `email` = '$email'") 
-     or die(mysql_error());
-    
-} else {*/
-
-
-  //  or die(mysql_error());  
-
-//}
 ?>
